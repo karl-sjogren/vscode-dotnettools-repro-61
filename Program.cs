0c6e8d8437b93f621625624a8c91100b49c26be2
Karl-Johan Sjögren <karl.sjogren@gmail.com>
2023-06-12 22:37:43 +0200

    Created simple repro

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AnalyzerProblems;
+
+public static class Program {
+    public static void Main() {
+        Console.WriteLine("Hello World!");
+    }
+
+    public static async Task WaitABit() {
+        await Task.Delay(1000);
+    }
+}
